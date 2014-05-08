aa9c2f3394571aefb3dc708248fe0fe359703675
Gearov <gearov@gmail.com>
2014-05-07 21:26:20 -0700

    Change Hello World message to Hello Azure.

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req,res) {
    res.writeHead(200, {'Content-Type':'http'});
-   res.end('Hello world');
+   res.end('Hello Azure');
 }).listen(port);
