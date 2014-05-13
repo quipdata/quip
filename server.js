510a4adb435657d9ad10d3e96340264b29740c71
Gearov <gearov@gmail.com>
2014-05-12 19:03:19 -0700

    mysql update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req,res) {
-   res.writeHead(200, {'Content-Type':'http'});
+   res.writeHead(200, {'Content-Type':'text/html'});
    res.end('Hello Azure');
 }).listen(port);
-console.log("Server running");
+console.log("Server running at port " + port);
