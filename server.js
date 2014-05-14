5562319498d8e79d9997f33ccb4c3ad683988688
Gearov <gearov@gmail.com>
2014-05-14 15:27:52 -0700

    Updated with routing.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var connection = mysql.createConnection({
 	user: 'bee741e5969e8c',
 	password: '8a2a5909'
 });
+
 connection.connect(function (err) {
 	if (err) {
 		console.error('error connecting: ' + err.stack);
@@ -24,24 +25,36 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 connection.end();
 
 
-var http = require('http')
+var http = require('http');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
+
+function serveStaticFile(res, path, contentType, responseCode) {
+	if(!responseCode) responseCode = 200;
+	fs.readFile(__dirname + path, function(err,data) {
+		if (err) {
+			res.writeHead(500, {'Content-Type': 'text/html'});
+			res.end('500 - Internal Error');
+		} else {
+			res.writeHead(responseCode, {'Content-Type': contentType });
+			res.end(data);
+		}
+	});
+}
 
 http.createServer(function(req,res) {
 	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
 
 	switch (path) {
 		case '':
-			res.writeHead(200,{'Content-Type':'text/html'});
-			res.end('Homepage. SQL response was: ' + sqlResponse);
+			serveStaticFile(res, '/public/index.html', 'text/html');
 			break;
 		case '/about':
-			res.writeHead(200,{'Content-Type':'text/html'});
-			res.end('About');
+			serveStaticFile(res, '/public/about.html', 'text/html');
 			break;
 		default:
-			res.writeHead(404,{'Content-Type':'text/html'});
-			res.end('Not found');
+			serveStaticFile(res, '/public/notfound.html', 'text/html', 404);
+			break;
 	}
 }).listen(port);
 
