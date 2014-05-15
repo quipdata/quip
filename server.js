8145c5fe52518f5a6b4c153c8598f0df4b629feb
Gearov <gearov@gmail.com>
2014-05-14 22:39:25 -0700

    End of night.

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
+// server.js
+
+// basic setup
 var express = require('express');
 var http = require('http');
-
 var app = express();
-
 var port = process.env.PORT || 1337;
+
 
 app.set('port', port );
 
-app.get('/', function(req,res) {
-	res.type('text/html');
+var router = express.Router();
+
+router.get('/', function(req,res) {
 	res.send('Hello, World');
 });
 
-app.get('/about', function(req,res) {
-	res.type('text/html');
+router.get('/about', function(req,res) {
 	res.send('About Quipdata');
 });
 
-app.use(function(req,res,next){
-	res.type('text/html');
+router.use(function(req,res,next){
 	res.status(404);
 	res.send('404-Not Found');
 });
+
+app.use('/', router);
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express started at ' + app.get('port') + '; press Ctrl-C to terminate.');
