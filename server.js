89451e71001ee99a5f6ce20c934959f9cad73cf9
Gearov <gearov@gmail.com>
2014-05-17 14:36:39 -0700

    Scaffolding established.

--- server.js
+++ server.js
@@ -6,25 +6,44 @@ var http = require('http');
 var app = express();
 var port = process.env.PORT || 1337;
 
+// handlebars is our current view engine, and res.render calls will be routed
+// to the handlebars engine
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+
 
 app.set('port', port );
 
+// The default router; handles non-secure transactions
 var router = express.Router();
 
 router.get('/', function(req,res) {
-	res.send('Hello, World');
+	res.render('home');
 });
 
+// Here we see an example of templating in action - we swap our randomFortune for
+// the appropriate handlebar code in about.handlebars, go check that out
 router.get('/about', function(req,res) {
-	res.send('About Quipdata');
+	var randomFortune = fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+	res.render('about', {fortune:randomFortune});
 });
 
 router.use(function(req,res,next){
 	res.status(404);
-	res.send('404-Not Found');
+	res.render('404');
 });
 
+app.use(express.static(__dirname + '/public'));
 app.use('/', router);
+
+var fortuneCookies = [
+	'be as water',
+	'reality is an illusion',
+	'when your mind speaks, be the one that hears',
+	'you are not your thoughts or feelings',
+	'die the death before death'
+];
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express started at ' + app.get('port') + '; press Ctrl-C to terminate.');
